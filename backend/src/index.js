104d6b5df3e9c533cdc72f187350c28766405b19
Roshan-504 <163719813+Roshan-504@users.noreply.github.com>
2025-02-05 15:31:57 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
